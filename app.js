1b94cfc3236739df993631c027f3d2df99d132ec
valeriaZerain <126807138+valeriaZerain@users.noreply.github.com>
2024-12-02 08:34:59 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, Jenkins!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app; // Para pruebas
